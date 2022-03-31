89d71d8f69f0903e50fbb53e08c93038bbb1eccd
ayukako <yukako1102n@gmail.com>
2022-03-31 10:28:13 +0000

    test

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -70,7 +70,8 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-config.hosts << "14466745a577458b87d8c467b68be211.vfs.cloud9.us-east-1.amazonaws.com"
+
+  config.hosts << "64c5a2e8a8d64702a7b0cec03e90743d.vfs.cloud9.us-east-1.amazonaws.com"
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
   config.active_job.queue_adapter = :inline
--- db/migrate/20220327070705_add_caption_to_post_images.rb
+++
@@ -1,7 +0,0 @@
-class AddCaptionToPostImages < ActiveRecord::Migration[6.1]
-  def change
-    add_column :post_images, :caption, :text
-    add_column :post_images, :shop_name, :string
-    add_column :post_images, :user_id, :integer
-  end
-end
